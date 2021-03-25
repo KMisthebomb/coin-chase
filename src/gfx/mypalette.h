d4955e46315e7f010be5adaf779fd4b76fdef9ab
Kota Mitchell <kota.m.mitchell@gmail.com>
2021-03-26 08:28:07 +0900

    Add files via upload

---
+++ src/gfx/mypalette.h
@@ -0,0 +1,15 @@
+#ifndef mypalette_include_file
+#define mypalette_include_file
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define sizeof_mypalette 26
+extern unsigned char mypalette[26];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
